f74d7d954969422c196f0006e58ddd0582092320
Kenny <88726944+Nnnsightnnn@users.noreply.github.com>
2023-03-30 17:24:44 +0000

    modified:   .gitignore 	new file:   __pycache__/llm_handler.cpython-310.pyc 	modified:   data_convert.py 	new file:   data_structure.py 	modified:   llm_handler.py 	new file:   static/7230.txt 	new file:   static/elements.pdf 	new file:   static/slatebook.txt 	new file:   static/split.txt 	new file:   text_split.

--- data_convert.py
+++ data_convert.py
@@ -1,9 +1,10 @@
 """This module gets .pdf file and returns structured data to plug into llm model"""
 
-import os
 from unstructured.partition.pdf import partition_pdf
 
+#structure pdf file
+elements = partition_pdf("static/7230.pdf", strategy="hi_res")
 
-elements = partition_pdf("static/slatebook2.pdf")
-
-print("\n\n".join([str(el) for el in elements]))
+"""create new file with elements"""
+write_file = open("static/elements.pdf", "w")
+write_file.write("\n\n".join([str(el) for el in elements]))
--- llm_handler.py
+++ llm_handler.py
@@ -1,10 +1,22 @@
 """This module gets user input and returns answer from OpenAI API"""
 
 import os
+import openai
 from langchain.llms import OpenAI
 
+
+def embed_text(elements):
+    """Your code to embed the text into a vector space"""
+    openai.api_key = os.environ.get("OPENAI_API_KEY")
+    elements = openai.Embedding.create(
+        input=elements,
+        model="text-embedding-ada-002"
+    )
+    return elements
+
+
 def communicate_with_llm(user_message):
-    # Your code to communicate with the language model (e.g., GPT-4)
+    """Your code to communicate with the language model (e.g., GPT-4)"""
     api_key = os.environ.get("OPENAI_API_KEY")
     llm = OpenAI(temperature=0.9, max_tokens=100, api_key=api_key)
 
@@ -12,3 +24,5 @@ def communicate_with_llm(user_message):
 
     # Return the response string (replace 'response' with the actual response variable if different)
     return response
+
+# Path: llm_handler.py
