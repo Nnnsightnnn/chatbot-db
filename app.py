029b5f7f45dd101e99fa81e11b4dfb5c202494c5
Kenny <88726944+Nnnsightnnn@users.noreply.github.com>
2023-03-31 16:25:38 +0000

    modified:   app.py 	modified:   llm_handler.py

--- app.py
+++ app.py
@@ -28,4 +28,4 @@ if __name__ == '__main__':
     app.run(debug=True)
 
 
-#...
+# path: app.py
--- llm_handler.py
+++ llm_handler.py
@@ -3,6 +3,7 @@ import openai
 from langchain.llms import OpenAI
 from data_structure import get_best_matching_document_content
 
+
 def embed_text(elements):
     """Your code to embed the text into a vector space"""
     openai.api_key = os.environ.get("OPENAI_API_KEY")
@@ -12,6 +13,7 @@ def embed_text(elements):
     )
     return elements
 
+
 def generate_summary(user_message, document_content):
     api_key = os.environ.get("OPENAI_API_KEY")
     llm = OpenAI(temperature=0.5, max_tokens=100, api_key=api_key)
@@ -20,27 +22,31 @@ def generate_summary(user_message, document_content):
     summary = llm(summary_prompt)
     return summary
 
+
 def communicate_with_llm(user_message):
     """Your code to communicate with the language model (e.g., GPT-4)"""
     api_key = os.environ.get("OPENAI_API_KEY")
     llm = OpenAI(temperature=0.9, max_tokens=100, api_key=api_key)
 
     document_content = get_best_matching_document_content(user_message)
+
     if document_content:
         summary = generate_summary(user_message, document_content)
-        new_prompt = (f"{user_message}\n\nBased on the summary of the relevant information:
-                  \n{summary}\n\nPlease provide an informed and natural response: ")
-else:
-    new_prompt = user_message
+        new_prompt = f"{user_message}\n\nBased on the summary of the relevant information:\n{summary}\n\nPlease provide an informed and natural response: "
+
+    else:
+        new_prompt = user_message
+
 
 response = llm(new_prompt)
+
 
 # Return the response string (replace 'response' with the actual response variable if different)
 return response
 
-    response = llm(user_message)
+response = llm(user_message)
 
-    # Return the response string (replace 'response' with the actual response variable if different)
-    return response
+# Return the response string (replace 'response' with the actual response variable if different)
+return response
 
 # Path: llm_handler.py
