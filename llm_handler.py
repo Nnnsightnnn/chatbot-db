9e7b2c38697d524a21714440cb5bcbc3ee3f09e5
Kenny <88726944+Nnnsightnnn@users.noreply.github.com>
2023-03-31 19:47:39 +0000

    modified:   llm_handler.py 	new file:   prompt_template.py

--- llm_handler.py
+++ llm_handler.py
@@ -20,7 +20,7 @@ def embed_text(elements):
 def generate_summary(document_content):
     """this function generates a summary from document content"""
     openapi_key = os.environ.get("OPENAI_API_KEY")
-    llm = OpenAI(temperature=0.2, max_tokens=300, api_key=openapi_key)
+    llm = OpenAI(temperature=0.3, max_tokens=300, api_key=openapi_key)
 
     summary_prompt = (f"Please provide a brief summary of the"
                       f"following information:\n{document_content}")
@@ -31,8 +31,8 @@ def generate_summary(document_content):
 def communicate_with_llm(user_message):
     """Your code to communicate with the language model (e.g., GPT-4)"""
 #initialize llm
-    api_key = os.environ.get("OPENAI_API_KEY")
-    llm = OpenAI(temperature=0.9, max_tokens=200, api_key=api_key)
+    openai_api_key = os.environ.get("OPENAI_API_KEY")
+    llm = OpenAI(temperature=0.5, max_tokens=200, api_key=openai_api_key)
 #create document content for llm
     document_content = get_best_matching_document_content(user_message)
 #logic for document content
