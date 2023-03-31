b9775d5c895fa800f108e21d33381cd811c85ff0
Kenny <88726944+Nnnsightnnn@users.noreply.github.com>
2023-03-31 16:15:49 +0000

    modified:   data_structure.py 	modified:   llm_handler.py 	renamed:    restrict_act.txt -> static/restrict_act.txt

--- data_structure.py
+++ data_structure.py
@@ -1,48 +1,34 @@
-"""This module splits data.txt then loads into Pinecone"""
 import os
 import pinecone
-
+from dotenv import load_dotenv
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import Pinecone
 from langchain.document_loaders import TextLoader
 from langchain.text_splitter import CharacterTextSplitter
 
-# initialize loader and load split text
-# loader = TextLoader("static/split.txt")
-# documents = loader.load()
+load_dotenv()
 
-# add openai_api_key to environment variables
 api_key = os.environ.get("OPENAI_API_KEY")
+api_key_pinecone = os.environ.get("PINECONE_API_KEY")
 
-
-# load and split text and send to docs variable
-
-from langchain.document_loaders import TextLoader
-loader = TextLoader('static/elements.pdf')
+loader = TextLoader('static/7230.txt')
 documents = loader.load()
 text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
-docs = text_splitter.split_documents(documents)
+document_chunks = text_splitter.split_documents(documents)
 
-# initialize of embeddings which transforms text into vectors
-embeddings = OpenAIEmbeddings(openai_api_key="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")
+embeddings = OpenAIEmbeddings(openai_api_key=api_key)
+index_name = "839567e8-a62f-4575-8acd-64693d352995"
 
-# initialize pinecone vector store
-pinecone.init(api_key=os.environ.get("PINECONE_API_KEY"),
-              environment="	us-central1-gcp"
-              )
+pinecone.init(api_key=api_key_pinecone, environment="us-central1-gcp")
 
-# create variable for index name
-INDEX_NAME = "slatebook001"
+docsearch = Pinecone.from_documents(document_chunks, embeddings, index_name=index_name)
 
-docsearch = Pinecone.from_documents(docs, embeddings, index_name=INDEX_NAME)
+def get_best_matching_document_content(query):
+    """This function returns the best matching document content based on the user's query."""
+    matching_docs = docsearch.similarity_search(query)
 
-# create a variable for the query
-#query = "What are employee rights?"
-
-# search for the query
-#docs = docsearch.similarity_search(query)
-
-# print the results of the query and page content
-#print(docs[0])
-
-# data_structure.py
+    if matching_docs:
+        best_document = matching_docs[0]
+        return best_document.page_content
+    else:
+        return None
--- llm_handler.py
+++ llm_handler.py
@@ -1,9 +1,7 @@
-"""This module gets user input and returns answer from OpenAI API"""
-
 import os
 import openai
 from langchain.llms import OpenAI
-
+from data_structure import get_best_matching_document_content
 
 def embed_text(elements):
     """Your code to embed the text into a vector space"""
@@ -14,11 +12,31 @@ def embed_text(elements):
     )
     return elements
 
+def generate_summary(user_message, document_content):
+    api_key = os.environ.get("OPENAI_API_KEY")
+    llm = OpenAI(temperature=0.5, max_tokens=100, api_key=api_key)
+
+    summary_prompt = f"Please provide a brief summary of the following information:\n{document_content}"
+    summary = llm(summary_prompt)
+    return summary
 
 def communicate_with_llm(user_message):
     """Your code to communicate with the language model (e.g., GPT-4)"""
     api_key = os.environ.get("OPENAI_API_KEY")
     llm = OpenAI(temperature=0.9, max_tokens=100, api_key=api_key)
+
+    document_content = get_best_matching_document_content(user_message)
+    if document_content:
+        summary = generate_summary(user_message, document_content)
+        new_prompt = (f"{user_message}\n\nBased on the summary of the relevant information:
+                  \n{summary}\n\nPlease provide an informed and natural response: ")
+else:
+    new_prompt = user_message
+
+response = llm(new_prompt)
+
+# Return the response string (replace 'response' with the actual response variable if different)
+return response
 
     response = llm(user_message)
 
