4c9be31f3eecad32bc48fe4e8ed172f585051bf2
Kenny <88726944+Nnnsightnnn@users.noreply.github.com>
2023-04-17 22:46:06 +0000

    modified:   agents/coder.py 	modified:   agents/local_db_embedding.py 	modified:   directory.txt 	modified:   requirements.txt

--- agents/coder.py
+++ agents/coder.py
@@ -1,9 +1,10 @@
-"""this module will take user input and return Python script"""
+"""
+This module will take user input and return Python script.
+"""
 
 import os
 import openai
 from dotenv import load_dotenv
-#from template_module  import TemplateClass
 from write_file import write_input_to_py_file
 from local_db_embedding import get_best_matching_document_content
 from langchain.chat_models import ChatOpenAI
@@ -12,72 +13,70 @@ import config
 
 load_dotenv()
 
-#initialize smart_llm
+
 def generate_code_alpha(user_message):
-    """this function utilizes document content to generate code"""
-#create document content for llm
+    """Utilizes document content to generate code."""
     document_content = get_best_matching_document_content(user_message)
-#initialize llm
     openai.api_key = os.environ.get("OPENAI_API_KEY")
-    llm = ChatOpenAI(temperature=0.3, model_name='gpt-3.5-turbo', max_tokens=4000, api_key=openai.api_key)
+    llm = ChatOpenAI(temperature=0.3, model_name='gpt-3.5-turbo',
+                     max_tokens=4000, api_key=openai.api_key)
     code_prompt = (f"Please generate code in Python to produce {user_message}"
-                      f"utilize these {document_content} as a primary reference when generating code")
+                   f"utilize these {document_content} as a primary reference when generating code")
     code_response = llm.call_as_llm(code_prompt)
     return code_response
+
 
 def generate_code_beta(user_message):
-    """this function utilizes document content to generate code"""
-#create document content for llm
-#    document_content = get_best_matching_document_content(user_message)
-#initialize llm
+    """Utilizes document content to generate code."""
     openai.api_key = os.environ.get("OPENAI_API_KEY")
-    llm = ChatOpenAI(temperature=0.3, model_name='gpt-3.5-turbo', max_tokens=4000, api_key=openai.api_key)
+    llm = ChatOpenAI(temperature=0.3, model_name='gpt-3.5-turbo',
+                     max_tokens=4000, api_key=openai.api_key)
     code_prompt = (f"Please review/improve this code {user_message}"
-                      f"Respond with the changes/improvements encapsulated with triple quotation marks and then the corrected version of the code")
+                   f"Respond with the changes/improvements encapsulated"
+                    f"with triple quotation marks and then the corrected version of the code")
     code_response = llm.call_as_llm(code_prompt)
     return code_response
+
 
 def generate_code_release(user_message):
-    """this function utilizes document content to generate code"""
-#create document content for llm
-#    document_content = get_best_matching_document_content(user_message)
-#initialize llm
+    """Utilizes document content to generate code."""
     openai.api_key = config.OPENAI_API_KEY
-    llm = ChatOpenAI(temperature=0.3, model_name='gpt-3.5-turbo', max_tokens=4000, api_key=openai.api_key)
+    llm = ChatOpenAI(temperature=0.3, model_name='gpt-3.5-turbo',
+                     max_tokens=4000, api_key=openai.api_key)
     code_prompt = (f"Please review/improve this code {user_message}"
-                      f"Respond with the changes/improvements encapsulated with triple quotation marks and then the corrected version of the code")
+                   f"Respond with the changes/improvements encapsulated"
+                    f"with triple quotation marks and then the corrected version of the code")
     code_response = llm.call_as_llm(code_prompt)
     return code_response
 
+
 def name_release(code_response):
-    """this function allows a LLM to look at the code and name it"""
-#initilize llm
+    """Allows a LLM to look at the code and name it."""
     openai.api_key = config.OPENAI_API_KEY
-    llm = ChatOpenAI(temperature=0.3, model_name='gpt-3.5-turbo', max_tokens=4000, api_key=openai.api_key)
+    llm = ChatOpenAI(temperature=0.3, model_name='gpt-3.5-turbo',
+                     max_tokens=4000, api_key=openai.api_key)
     code_prompt = (f"Please name this release {code_response}"
-                        f"Respond with the name of the release")
+                   f"Respond with the name of the release")
     release_name = llm.call_as_llm(code_prompt)
     return release_name
 
+
 def long_running_operation():
-    # Do some long-running operation here
-    for i in range(1000000):
+    """Performs a long-running operation."""
+    for _ in range(1000000):
         pass
 
-def main(user_message):
-    """this function will take user input and return Python script"""
 
+def main(user_message):
+    """Takes user input and returns Python script."""
     with tqdm(total=1) as progress:
         long_running_operation()
         progress.update(1)
 
-        #generate code_alpha
         code_alpha = generate_code_alpha(str(user_message))
-        #json_description_of_code = smart_llm.call(template_class.generate_json(user_input))
 
-        ### template_class.beta
         code_beta = generate_code_beta(str(code_alpha))
-        ### template_class.release
+
         code_release = generate_code_release(str(code_beta))
         release_name = name_release(str(code_release))
         write_input_to_py_file(code_alpha, f"{release_name}_alpha.py")
@@ -85,4 +84,3 @@ def main(user_message):
         write_input_to_py_file(code_release, f"{release_name}_release.py")
 
     print("Finished")
-#Path: agents\coder.py
--- agents/local_db_embedding.py
+++ agents/local_db_embedding.py
@@ -1,5 +1,6 @@
+"""this module creates an embedding utilizing an Ada-GPT model and upload to Pinecone."""
+
 import os
-import config
 from typing import List, Union
 import pinecone
 from dotenv import load_dotenv
@@ -8,11 +9,14 @@ from langchain.vectorstores import Pinecone
 from langchain.document_loaders import TextLoader
 from langchain.text_splitter import CharacterTextSplitter
 
+import config
 
 load_dotenv()
 
 
-print(f"Found files in {config.DATABASE_DIRECTORY} and {config.CHUNK_SIZE} directory.")
+print(
+    f"Found files in {config.DATABASE_DIRECTORY} and {config.CHUNK_SIZE} directory.")
+
 
 def get_text_loaders(directory: str) -> List[TextLoader]:
     """Retrieve a list of TextLoader objects from a given directory."""
@@ -22,13 +26,18 @@ def get_text_loaders(directory: str) -> List[TextLoader]:
         if os.path.isfile(os.path.join(directory, filename))
     ]
 
-def main():
-    embeddings = OpenAIEmbeddings(openai_api_key=config.OPENAI_API_KEY)
-    pinecone.init(api_key=config.PINECONE_API_KEY, environment=config.PINECONE_ENVIRONMENT)
 
-    text_splitter = CharacterTextSplitter(chunk_size=int(config.CHUNK_SIZE), chunk_overlap=int(config.CHUNK_OVERLAP))
+def main():
+    """Create an embedding and upload it to Pinecone."""
+    embeddings = OpenAIEmbeddings(openai_api_key=config.OPENAI_API_KEY)
+    pinecone.init(api_key=config.PINECONE_API_KEY,
+                  environment=config.PINECONE_ENVIRONMENT)
+
+    text_splitter = CharacterTextSplitter(chunk_size=int(config.CHUNK_SIZE),
+                                          chunk_overlap=int(config.CHUNK_OVERLAP))
     loaders = get_text_loaders(config.DATABASE_DIRECTORY)
-    print(f"Found {len(loaders)} files in {config.DATABASE_DIRECTORY} directory.")
+    print(
+        f"Found {len(loaders)} files in {config.DATABASE_DIRECTORY} directory.")
 
     docsearch = None
 
@@ -37,7 +46,8 @@ def main():
         document_chunks = text_splitter.split_documents(documents)
 
         if docsearch is None:
-            docsearch = Pinecone.from_documents(document_chunks, embeddings, index_name=config.INDEX_NAME, upsert=True)
+            docsearch = Pinecone.from_documents(
+                document_chunks, embeddings, index_name=config.INDEX_NAME, upsert=True)
         else:
             docsearch.add_documents(document_chunks, upsert=True)
 
@@ -48,7 +58,7 @@ docsearch_instance = main()
 
 
 def get_best_matching_document_content(question: str) -> Union[str, None]:
-    """Return the best matching document content based on the user's query."""   
+    """Return the best matching document content based on the user's query."""
     matching_docs = docsearch_instance.similarity_search(question)
 
     if matching_docs:
