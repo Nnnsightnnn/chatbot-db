6dcf9a68be27dd0e28d87e03ed011e0fa63fb9a8
Kenny <88726944+Nnnsightnnn@users.noreply.github.com>
2023-04-26 15:17:28 +0000

    modified:   .env.temp 	modified:   vector_store/clear_pinecone_data.py

--- vector_store/clear_pinecone_data.py
+++ vector_store/clear_pinecone_data.py
@@ -1,19 +1,16 @@
-import os
+"""Clear the Pinecone index."""
 import pinecone
 from dotenv import load_dotenv
 
+import config
+
 load_dotenv()
 
-PINECONE_API_KEY = os.environ.get("PINECONE_API_KEY")
-PINECONE_ENVIRONMENT = "us-central1-gcp"
-INDEX_NAME = "auto-gpt"
-
-def clear_pinecone_data(index_name: str):
-    pinecone.init(api_key=PINECONE_API_KEY, environment=PINECONE_ENVIRONMENT)
-    pinecone.deinit()
-    
-    with pinecone.deployment.PineconeIndex(index_name=index_name, api_key=PINECONE_API_KEY) as index:
-        index.delete()
+def clear_pinecone_data():
+    """Clear the Pinecone index."""
+    pinecone.init(api_key=config.PINECONE_API_KEY, environment=config.PINECONE_ENVIRONMENT)
+    pinecone.delete_index(config.INDEX_NAME)
+    print(f"Deleted index {config.INDEX_NAME}")
 
 if __name__ == "__main__":
-    clear_pinecone_data(INDEX_NAME)
+    clear_pinecone_data()
