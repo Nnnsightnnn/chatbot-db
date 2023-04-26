75d1db950bae55089a2b40b53632d6f4aa1bee13
NnnsightnnN <kjautry@gmail.com>
2023-04-26 13:06:18 -0400

    modified:   .gitignore 	modified:   agents/__pycache__/chat_controller.cpython-310.pyc 	new file:   agents/__pycache__/db_embedding.cpython-310.pyc 	new file:   agents/__pycache__/doc_search.cpython-310.pyc 	modified:   data_convert.py 	deleted:    database/elder_evils.txt 	deleted:    static/7230.pdf 	deleted:    static/7230.txt 	deleted:    static/API Documentation _ Alpha Vantage.html 	deleted:    static/API_reference.pdf 	deleted:    static/API_reference.txt 	deleted:    static/API_reference0.txt 	new file:   static/background_forest.jpg 	deleted:    static/chatbot_droids.jpg 	deleted:    static/elder_evils.pdf 	deleted:    static/elements.pdf 	deleted:    static/giant.jpg 	deleted:    static/restrict_act.txt 	deleted:    static/slatebook.pdf 	deleted:    static/slatebook.txt 	deleted:    static/slatebook2.pdf 	deleted:    static/snow_globe.jpg 	deleted:    static/split.txt

--- data_convert.py
+++ data_convert.py
@@ -1,17 +1,38 @@
-"""This module gets .pdf file and returns structured data to plug into llm model"""
-
+import os
+from io import StringIO
 from unstructured.partition.pdf import partition_pdf
-from unstructured.partition.text import partition_text
 from unstructured.cleaners.core import clean_non_ascii_chars
 
+# Define the input directory and output directory paths
+input_directory_path = "input/"
+output_directory_path = "output/"
 
-#structure pdf file
-# elements = partition_pdf("static/elder_evils", strategy="hi-res")
-# elements = partition_pdf("static/elder_evils.pdf", strategy="hi-res")
-elements = partition_text("static/elder_evils.txt", encoding="utf-8")
+# Get the list of files in the input directory
+file_list = os.listdir(input_directory_path)
 
-elements = clean_non_ascii_chars(str(elements))
+# Process each file in the input directory
+for file_name in file_list:
+    # Check if the file is a PDF
+    if file_name.endswith(".pdf"):
+        input_file_path = os.path.join(input_directory_path, file_name)
+        output_file_path = os.path.join(output_directory_path, f"{file_name[:-4]}_cleaned.txt")
 
-"""create new file with elements"""
-write_file = open("static/elder_evils.txt", "w", encoding="utf-8")
-write_file.write("\n\n".join([str(el) for el in elements]))
+        # Structure PDF file
+        elements = partition_pdf(input_file_path)
+
+        # Write the elements into a StringIO object
+        elements_str = StringIO()
+        elements_str.write("\n\n".join([str(el) for el in elements]))
+
+        # Read the contents of the StringIO object
+        elements_str.seek(0)
+        file_content = elements_str.read()
+
+        # Apply the clean_non_ascii_chars function on the file content
+        cleaned_content = clean_non_ascii_chars(file_content)
+
+        # Write the cleaned content to the output file
+        with open(output_file_path, "w", encoding="utf-8") as output_file:
+            output_file.write(cleaned_content)
+
+        print(f"Non-ASCII characters have been successfully removed from {file_name} and the cleaned content saved to {output_file_path}")
